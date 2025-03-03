17bca3acdd50ded0478c09b96d1ba4d15295c52c
Aldair0308 <al222111405@gmial.com>
2025-03-02 18:43:13 -0600

    function try four

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as crypto from 'crypto';
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
